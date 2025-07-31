ed5dbbf5607f6593bac5078f2f78c7a324b10aba
caosaikang <caosaikang@outlook.com>
2025-07-31 15:08:29 +0800

    Update config.rs
    修改服务器和key

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["anomecs.top"];
+pub const RS_PUB_KEY: &str = "AAAAC3NzaC1lZDI1NTE5AAAAIMhOpc97wyR8Q0ZDrUBZY1ldgx+2e5wH7fsawyBf08pz";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
